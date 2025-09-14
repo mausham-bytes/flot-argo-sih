68eb1af484b6dc2c70d1192acc4c20163e6b1c21
Arpit Singh <asr827013@gmail.com>
2025-09-14 21:01:52 +0530

    updated API variable

--- Argo_backend/config.py
+++ Argo_backend/config.py
@@ -8,7 +8,7 @@ class Config:
     MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/argo_db")
 
     # DeepSeek API Key
-    DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
+    LLM_API_KEY = os.getenv("LLM_API_KEY")
 
     # Optional: Vector DB path
     VECTOR_DB_PATH = os.getenv("VECTOR_DB_PATH", "./chroma_store")
